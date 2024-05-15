2ba844943f3194121546d9ec0671345caeacbb96
wjkba <63542318+wjkba@users.noreply.github.com>
2024-05-15 08:24:59 +0200

    update
    - working on new add recipe form

--- frontend/src/Components/AddRecipe/AddRecipeForm.jsx
+++ frontend/src/Components/AddRecipe/AddRecipeForm.jsx
@@ -1,142 +1,118 @@
-import axios from "axios";
-import { useForm } from "react-hook-form";
+import { useEffect, useState, useRef } from "react";
+import { RiCloseLine } from "react-icons/ri";
 
 export default function AddRecipeForm() {
-  const {
-    register,
-    handleSubmit,
-    setError,
-    formState: { errors, isSubmitting },
-  } = useForm();
+  const [ingredients, setIngredients] = useState([{ ingredient: "" }]);
+  const inputRef = useRef();
 
-  const onSubmit = async (data) => {
-    axios
-      .post("http://localhost:8000/api/recipe", {
-        title: data.title,
-        image: data.image,
-        description: data.description,
-        time: data.time,
-        ingredients: data.ingredients,
-        instructions: data.instructions,
-      })
-      .then((res) => console.log(res))
-      .catch((error) => {
-        console.error(error);
-        setError("root", { message: error.message });
-      });
-    console.log(data);
+  const handleValueChange = (index, event) => {
+    const values = [...ingredients];
+    values[index].ingredient = event.target.value;
+    setIngredients(values);
   };
 
-  // todo: add react hook form
+  const handleAddInput = (index) => {
+    const values = [...ingredients];
+    if (values[index].ingredient !== "") {
+      values.push({ ingredient: "" });
+      setIngredients(values);
+    }
+  };
+  const handleDeleteInput = (event, index) => {
+    event.preventDefault();
+    const values = [...ingredients];
+    if (values.length > 1) {
+      values.splice(index, 1);
+      setIngredients(values);
+    }
+  };
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, [ingredients]);
 
   return (
-    <form className="max-w-[15rem] w-full" onSubmit={handleSubmit(onSubmit)}>
-      <div className="flex flex-col ">
-        <div className="mb-2">
-          <label>
-            Title
-            <input
-              {...register("title", {
-                required: "Title is required",
-              })}
-              type="text"
-              className="border w-full"
-            />
-          </label>
-          {errors.title && (
-            <p className="text-sm text-red-400">{errors.title.message}</p>
-          )}
+    <>
+      <button onClick={() => console.log(ingredients)}>xxx</button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className="lg:max-w-[20rem] w-full"
+      >
+        <div className="flex flex-col ">
+          <div className="mb-2 ">
+            <label>
+              Title
+              <input type="text" className="border w-full" />
+            </label>
+          </div>
+          <div className="mb-2">
+            <label>
+              Description
+              <textarea
+                type="text"
+                className="resize-none border min-h-[5rem] w-full"
+              />
+            </label>
+          </div>
+          <div className="mb-2">
+            <label>
+              Cooking time
+              <input type="text" className="border w-full" />
+            </label>
+          </div>
+          <div className="mb-2">
+            <label>
+              Ingredients
+              <ul>
+                {ingredients.map((ingredient, index) => (
+                  <li key={index} className="flex mb-2">
+                    <input
+                      onKeyDown={(event) => {
+                        if (event.key === "Enter") {
+                          handleAddInput(index);
+                        }
+                      }}
+                      type="text"
+                      className="outline-none border w-full"
+                      onChange={(e) => handleValueChange(index, e)}
+                      value={ingredient.ingredient}
+                      ref={index === ingredients.length - 1 ? inputRef : null}
+                    />
+                    <button
+                      type="button"
+                      onClick={(e) => handleDeleteInput(e, index)}
+                      className="px-4 bg-szpgray"
+                    >
+                      <RiCloseLine />
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </label>
+          </div>
+          <div className="mb-2">
+            <label>
+              Instructions
+              <input type="text" className="border w-full" />
+            </label>
+          </div>
         </div>
 
         <div className="mb-2 ">
           <label>
             Image
-            <input
-              {...register("image", {
-                required: "Image link is required",
-              })}
-              type="text"
-              className="border w-full"
-            />
+            <input type="text" className="border w-full" />
           </label>
-          {errors.image && (
-            <p className="text-sm text-red-400">{errors.image.message}</p>
-          )}
         </div>
-        <div className="mb-2">
-          <label>
-            Description
-            <input
-              {...register("description", {
-                required: "Description is required",
-              })}
-              type="text"
-              className="border w-full"
-            />
-          </label>
-          {errors.description && (
-            <p className="text-sm text-red-400">{errors.description.message}</p>
-          )}
-        </div>
-
-        <div className="mb-2">
-          <label>
-            Time
-            <input
-              {...register("time", {
-                required: "Time is required",
-              })}
-              type="text"
-              className="border w-full"
-            />
-          </label>
-          {errors.time && (
-            <p className="text-sm text-red-400">{errors.time.message}</p>
-          )}
-        </div>
-        <div className="mb-2">
-          <label>
-            Ingredients
-            <input
-              {...register("ingredients", {
-                required: "Ingredients are required",
-              })}
-              type="text"
-              className="border w-full"
-            />
-          </label>
-          {errors.ingredients && (
-            <p className="text-sm text-red-400">{errors.ingredients.message}</p>
-          )}
-        </div>
-        <div className="mb-2">
-          <label>
-            Instructions
-            <input
-              {...register("instructions", {
-                required: "Instructions are required",
-              })}
-              type="text"
-              className="border w-full"
-            />
-          </label>
-          {errors.instructions && (
-            <p className="text-sm text-red-400">
-              {errors.instructions.message}
-            </p>
-          )}
-        </div>
-      </div>
-      <button
-        disabled={isSubmitting}
-        type="submit"
-        className="hover:bg-szpgreen rounded bg-szppurple text-white w-full border p-2 px-8 "
-      >
-        {isSubmitting ? "Submitting..." : "Submit"}
-      </button>
-      {errors.root && (
-        <p className="text-sm text-red-400">{errors.root.message}</p>
-      )}
-    </form>
+        <button
+          type="submit"
+          className="hover:bg-szpgreen rounded bg-szppurple text-white w-full border p-2 px-8 "
+        >
+          Submit
+        </button>
+      </form>
+    </>
   );
 }
